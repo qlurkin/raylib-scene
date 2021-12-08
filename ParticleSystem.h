2a56bb25e49db3fd1dae55d6a6e8821ec7e8f7bd
Quentin Lurkin <qlurkin@gmail.com>
2021-12-08 10:41:47 +0100

    Add Particle Constructor

--- ParticleSystem.h
+++ ParticleSystem.h
@@ -14,9 +14,11 @@ class ParticleSystem : public Object {
         size_t count;
         std::vector<Force*> forces;
         std::vector<Collider*> colliders;
+        void allocate(size_t count);
 
     public:
         ParticleSystem(size_t count, Vector3 position, float deviation, Vector3 velocity, float velocityDeviation);
+        ParticleSystem(Vector3* positions, Vector3* velocities, size_t count);
         virtual void draw(Matrix matModel);
         void update(float dt);
         void addForce(Force *force);
